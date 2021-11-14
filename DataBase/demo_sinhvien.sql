5ffa56d9265154bcd9c2612e72c826cae9317b94
nguyen viet <lonconbaby2210@gmail.com>
2021-11-14 08:28:28 +0700

    Message

---
+++ DataBase/demo_sinhvien.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: demo
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sinhvien`
+--
+
+DROP TABLE IF EXISTS `sinhvien`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sinhvien` (
+  `Masv` int NOT NULL,
+  `Hovaten` varchar(45) DEFAULT NULL,
+  `Ngaysinh` date DEFAULT NULL,
+  `Gioitinh` varchar(45) DEFAULT NULL,
+  `Diachi` varchar(45) DEFAULT NULL,
+  `Sodienthoai` int DEFAULT NULL,
+  `MaLop` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Masv`),
+  KEY `Malop_idx` (`MaLop`),
+  CONSTRAINT `Malop` FOREIGN KEY (`MaLop`) REFERENCES `lop` (`MaLop`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sinhvien`
+--
+
+LOCK TABLES `sinhvien` WRITE;
+/*!40000 ALTER TABLE `sinhvien` DISABLE KEYS */;
+INSERT INTO `sinhvien` VALUES (119000703,'1','1991-01-01','Nam','1',1,'1'),(119000705,'NguyenQuocviet','2001-10-05','Nam','1',1,'1');
+/*!40000 ALTER TABLE `sinhvien` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-13 10:24:08
